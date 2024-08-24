c24fda9e0ca78adf5a32a69d2d4f226796ee1d7b
amelkumyandev <109224475+amelkumyandev@users.noreply.github.com>
2024-08-24 14:11:49 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using RsaSha256Signature.Utils;
+
+// Step 1: Generate RSA Keys in PEM format
+KeyGenerator.GenerateKeys();
+
+// The message to be signed
+string message = "Hello, World!";
+
+// Step 2: Sign the message
+string signature = Signer.SignMessage(message);
+Console.WriteLine("Signature: " + signature);
+
+// Step 3: Verify the signature
+bool isValid = Verifier.VerifySignature(message, signature);
+Console.WriteLine("Is the signature valid? " + isValid);
